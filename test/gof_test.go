840d4590818fb82449c97d8da2285c71a88f33f2
zhuhualiang <zhuhualiang@4399inc.com>
2022-12-09 10:54:15 +0800

    函数反射

--- test/gof_test.go
+++ test/gof_test.go
@@ -1,10 +1,10 @@
 package test
 
 import (
+	"github.com/gogf/gf/v2/frame/g"
+	"reflect"
 	"sync"
 	"testing"
-
-	"github.com/gogf/gf/v2/frame/g"
 
 	. "github.com/gogf/gf/v2/test/gtest"
 )
@@ -25,15 +25,20 @@ type (
 
 func Test_Async(t *testing.T) {
 	C(t, func(t *T) {
-		mm := RWMutex{}
-		mm.RWMutex = new(sync.RWMutex)
-		var o = obj{
-			as: &async{mu: mm},
-		}
-		if o.as.mu.RWMutex != nil {
-			g.Dump("lock")
-			return
-		}
-		g.Dump("unlock")
+		//mm := RWMutex{}
+		//mm.RWMutex = new(sync.RWMutex)
+		//var o = obj{
+		//	as: &async{mu: mm},
+		//}
+		//if o.as.mu.RWMutex != nil {
+		//	g.Dump("lock")
+		//	return
+		//}
+		//g.Dump("unlock")
+
+		type MyInt int
+		var x MyInt = 7
+		v := reflect.ValueOf(x)
+		g.Dump(v.Type())
 	})
 }
